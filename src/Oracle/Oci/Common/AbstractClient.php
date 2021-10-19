6faf3b552e949d78edf34d1a3f325d99b22af906
Mathias Ricken <mathias.ricken@oracle.com>
2021-10-19 11:37:39 -0700

    Making abstract client work.

--- src/Oracle/Oci/Common/AbstractClient.php
+++ src/Oracle/Oci/Common/AbstractClient.php
@@ -11,14 +11,17 @@ use InvalidArgumentException;
 
 abstract class AbstractClient 
 {
-    protected /*AuthProviderInterface*/ $auth_provider;
-    protected $region;
+    private /*AuthProviderInterface*/ $auth_provider;
+    protected /*?Region*/ $region;
 
+    protected /*string*/ $endpoint;
     protected $client;
-    
+
     public function __construct(
+        $endpointTemplate,
         AuthProviderInterface $auth_provider,
-        /*string*/ $region=null)
+        $region=null,
+        $endpoint=null)
     {
         $this->auth_provider = $auth_provider;
 
@@ -26,64 +29,95 @@ abstract class AbstractClient
         {
             $this->region = $auth_provider->getRegion();
         }
-        if ($region != null) 
+        if ($region != null)
         {
-            $this->region = $region;
+            if ($region instanceof Region)
+            {
+                $this->region = $region;
+            }
+            else {
+                $knownRegion = Region::getRegion($region);
+                if ($knownRegion == null)
+                {
+                    // forward-compatibility for unknown regions
+                    $realm = Realm::getRealmForUnknownRegion();
+                    $endpoint = str_replace('{region}', $region, $endpointTemplate);
+                    $endpoint = str_replace('{secondLevelDomain}', $realm->getRealmDomainComponent(), $endpoint);
+                    $this->region = null;
+                    // echo "Region $region is unknown, assuming it to be in realm $realm. Setting endpoint to $endpoint".PHP_EOL;
+                }
+                else
+                {
+                    $this->region = $knownRegion;
+                }
+            }
         }
-        if ($this->region == null) 
+        if ($this->region == null && $endpoint == null)
         {
-            throw new InvalidArgumentException('Region not set.');
+            throw new InvalidArgumentException('Neither region nor endpoint is set.');
         }
+
+        if ($endpoint != null) {
+            $this->endpoint = $endpoint;
+        }
+        else {
+            $this->endpoint = str_replace('{region}', $this->region->getRegionId(), $endpointTemplate);
+            $this->endpoint = str_replace('{secondLevelDomain}', $this->region->getRealm()->getRealmDomainComponent(), $this->endpoint);
+        }
+        // echo "Final endpoint: {$this->endpoint}".PHP_EOL;
 
         $handler = new CurlHandler();
         $stack = HandlerStack::create($handler);
 
         // place signing middleware after prepare-body so it can access Content-Length header
         $stack->after('prepare_body', Middleware::mapRequest(function (RequestInterface $request) {
+            // echo "Request URI: " . $request->getUri() . PHP_EOL;
+
             // headers required for all HTTP verbs
             $headers = "date (request-target) host";
-        
+
             // example: Thu, 05 Jan 2014 21:31:40 GMT
             $date=gmdate("D, d M Y H:i:s T", time());
             $method = strtolower($request->getMethod());
             $request_target = $request->getRequestTarget();
             $host = $request->getHeader('Host')[0];
-        
+
             $request = $request->withHeader('Date', $date);
-        
+
             $signing_string = "date: $date\n(request-target): $method $request_target\nhost: $host";
-        
+
             // additional required headers for POST and PUT requests
             if ($method == 'post' || $method == 'put') {
-                $content_length = $request->getHeader('Content-Length')[0];
-        
-                // if content length is 0 we still need to explicitly send the Content-Length header
-                if (!$content_length){
+                $clHeaders = $request->getHeader('Content-Length');
+                if ($clHeaders != null && count($clHeaders) > 0) {
+                    $content_length = $clHeaders[0];
+                } else {
+                    // if content length is 0 we still need to explicitly send the Content-Length header
                     $content_length = 0;
                     $request = $request->withHeader('Content-Length', 0);
                 }
-        
+
                 $content_type = $request->getHeader('Content-Type')[0];
                 $content_sha256 = base64_encode(hex2bin(hash("sha256", $request->getBody())));
-        
+
                 $request = $request->withHeader('x-content-sha256', $content_sha256);
-        
+
                 $headers = $headers . " content-length content-type x-content-sha256";
                 $signing_string = $signing_string . "\ncontent-length: $content_length\ncontent-type: $content_type\nx-content-sha256: $content_sha256";
             }
-        
-            # echo "Signing string:\n$signing_string".PHP_EOL;
-        
+
+            // echo "Signing string:\n$signing_string".PHP_EOL;
+
             $signature = $this->sign_string($signing_string, $this->auth_provider->getKeyFilename(), $this->auth_provider->getKeyPassphrase());
-        
+
             $authorization_header = "Signature version=\"1\",keyId=\"{$this->auth_provider->getKeyId()}\",algorithm=\"rsa-sha256\",headers=\"$headers\",signature=\"$signature\"";
             $request = $request->withHeader('Authorization', $authorization_header);
-            
-            # echo "\nRequest headers:".PHP_EOL;
-            # foreach ($request->getHeaders() as $name => $values) {
-            #     echo $name . ': ' . implode(', ', $values) . "\n";
-            # }
-        
+
+            // echo "\nRequest headers:".PHP_EOL;
+            // foreach ($request->getHeaders() as $name => $values) {
+            //     echo $name . ': ' . implode(', ', $values) . "\n";
+            // }
+
             return $request;
         }));
 
@@ -97,27 +131,10 @@ abstract class AbstractClient
         if (!$pkeyid) {
             exit('Error reading private key');
         }
-    
+
         openssl_sign($data, $signature, $pkeyid, OPENSSL_ALGO_SHA256);
-    
+
         return base64_encode($signature);
-    }
-
-    public function getNamespace()
-    {
-        $response = $this->client->get("https://objectstorage.{$this->region}.oraclecloud.com/n", [ 'headers' => ['Content-Type' => 'application/json']]);
-
-        $body = $response->getBody();
-        if (str_starts_with($body, "\"") && str_ends_with($body, "\"")) 
-        {
-            $body = substr($body, 1, -1);
-        }
-
-        return new OciResponse(
-            $response->getStatusCode(),
-            $response->getHeaders(),
-            $body,
-            null);
     }
 }
 ?>
--- src/Oracle/Oci/ObjectStorage/ObjectStorageClient.php
+++ src/Oracle/Oci/ObjectStorage/ObjectStorageClient.php
@@ -5,145 +5,27 @@
 namespace Oracle\Oci\ObjectStorage;
 
 use InvalidArgumentException;
-use Psr\Http\Message\RequestInterface;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Handler\CurlHandler;
-use GuzzleHttp\Client;
-use GuzzleHttp\Middleware;
 use Oracle\Oci\Common\AuthProviderInterface;
 use Oracle\Oci\Common\HttpUtils;
 use Oracle\Oci\Common\OciResponse;
-use Oracle\Oci\Common\Realm;
-use Oracle\Oci\Common\Region;
-use Oracle\Oci\Common\RegionProvider;
 use Oracle\Oci\Common\UserAgent;
+use Oracle\Oci\Common\AbstractClient;
 
-class ObjectStorageClient
+class ObjectStorageClient extends AbstractClient
 {
-    private /*AuthProviderInterface*/ $auth_provider;
-    protected /*?Region*/ $region;
-
     static protected /*const*/ $endpointTemplate = "https://objectstorage.{region}.{secondLevelDomain}";
-    protected /*string*/ $endpoint;
-    protected $client;
 
     public function __construct(
         AuthProviderInterface $auth_provider,
         $region=null,
         $endpoint=null)
     {
-        $this->auth_provider = $auth_provider;
-
-        if ($auth_provider instanceof RegionProvider)
-        {
-            $this->region = $auth_provider->getRegion();
-        }
-        if ($region != null)
-        {
-            if ($region instanceof Region)
-            {
-                $this->region = $region;
-            }
-            else {
-                $knownRegion = Region::getRegion($region);
-                if ($knownRegion == null)
-                {
-                    // forward-compatibility for unknown regions
-                    $realm = Realm::getRealmForUnknownRegion();
-                    $endpoint = str_replace('{region}', $region, ObjectStorageClient::$endpointTemplate);
-                    $endpoint = str_replace('{secondLevelDomain}', $realm->getRealmDomainComponent(), $endpoint);
-                    $this->region = null;
-                    // echo "Region $region is unknown, assuming it to be in realm $realm. Setting endpoint to $endpoint".PHP_EOL;
-                }
-                else
-                {
-                    $this->region = $knownRegion;
-                }
-            }
-        }
-        if ($this->region == null && $endpoint == null)
-        {
-            throw new InvalidArgumentException('Neither region nor endpoint is set.');
-        }
-
-        if ($endpoint != null) {
-            $this->endpoint = $endpoint;
-        }
-        else {
-            $this->endpoint = str_replace('{region}', $this->region->getRegionId(), ObjectStorageClient::$endpointTemplate);
-            $this->endpoint = str_replace('{secondLevelDomain}', $this->region->getRealm()->getRealmDomainComponent(), $this->endpoint);
-        }
-        // echo "Final endpoint: {$this->endpoint}".PHP_EOL;
-
-        $handler = new CurlHandler();
-        $stack = HandlerStack::create($handler);
-
-        // place signing middleware after prepare-body so it can access Content-Length header
-        $stack->after('prepare_body', Middleware::mapRequest(function (RequestInterface $request) {
-            // echo "Request URI: " . $request->getUri() . PHP_EOL;
-
-            // headers required for all HTTP verbs
-            $headers = "date (request-target) host";
-
-            // example: Thu, 05 Jan 2014 21:31:40 GMT
-            $date=gmdate("D, d M Y H:i:s T", time());
-            $method = strtolower($request->getMethod());
-            $request_target = $request->getRequestTarget();
-            $host = $request->getHeader('Host')[0];
-
-            $request = $request->withHeader('Date', $date);
-
-            $signing_string = "date: $date\n(request-target): $method $request_target\nhost: $host";
-
-            // additional required headers for POST and PUT requests
-            if ($method == 'post' || $method == 'put') {
-                $clHeaders = $request->getHeader('Content-Length');
-                if ($clHeaders != null && count($clHeaders) > 0) {
-                    $content_length = $clHeaders[0];
-                } else {
-                    // if content length is 0 we still need to explicitly send the Content-Length header
-                    $content_length = 0;
-                    $request = $request->withHeader('Content-Length', 0);
-                }
-
-                $content_type = $request->getHeader('Content-Type')[0];
-                $content_sha256 = base64_encode(hex2bin(hash("sha256", $request->getBody())));
-
-                $request = $request->withHeader('x-content-sha256', $content_sha256);
-
-                $headers = $headers . " content-length content-type x-content-sha256";
-                $signing_string = $signing_string . "\ncontent-length: $content_length\ncontent-type: $content_type\nx-content-sha256: $content_sha256";
-            }
-
-            // echo "Signing string:\n$signing_string".PHP_EOL;
-
-            $signature = $this->sign_string($signing_string, $this->auth_provider->getKeyFilename(), $this->auth_provider->getKeyPassphrase());
-
-            $authorization_header = "Signature version=\"1\",keyId=\"{$this->auth_provider->getKeyId()}\",algorithm=\"rsa-sha256\",headers=\"$headers\",signature=\"$signature\"";
-            $request = $request->withHeader('Authorization', $authorization_header);
-
-            // echo "\nRequest headers:".PHP_EOL;
-            // foreach ($request->getHeaders() as $name => $values) {
-            //     echo $name . ': ' . implode(', ', $values) . "\n";
-            // }
-
-            return $request;
-        }));
-
-        $this->client = new Client([
-            'handler' => $stack
-        ]);
-    }
-
-    protected function sign_string($data, $key_path, $passphrase){
-        $pkeyid = openssl_pkey_get_private($key_path, $passphrase);
-        if (!$pkeyid) {
-            exit('Error reading private key');
-        }
-
-        openssl_sign($data, $signature, $pkeyid, OPENSSL_ALGO_SHA256);
-
-        return base64_encode($signature);
+        parent::__construct(
+            ObjectStorageClient::$endpointTemplate,
+            $auth_provider,
+            $region,
+            $endpoint
+        );
     }
 
 
