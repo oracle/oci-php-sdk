2d2a6c59f47ce2efef008b8f1775e1458b946a6c
Mathias Ricken <mathias.ricken@oracle.com>
2021-10-20 11:16:22 -0700

    Adding unit testing to verify script.

--- tools/verify.sh
+++ tools/verify.sh
@@ -2,7 +2,10 @@
 
 scriptDir=`dirname $0`
 
+$scriptDir/../vendor/bin/phpunit --bootstrap vendor/autoload.php tests
+
 if $scriptDir/php-cs-fixer fix | grep -E "^ *[1-9][0-9]*) (src|test)"; then
     echo "Formatting changes found, aborting commit..."
     exit 1
 fi
+
